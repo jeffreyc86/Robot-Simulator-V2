8603f711f20802cecf800d292fe27f1be81bee68
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-01-13 15:21:03 -0500

    complete with setInterval

--- index.js
+++ index.js
@@ -2,5 +2,53 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
+ 
   // ADD CODE HERE!
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft"){
+      addMove("left")
+    }
+    else if (e.key === "ArrowRight"){
+      addMove("right")
+    }
+    else if (e.key === "ArrowUp"){
+      addMove("up")
+    }
+    else if (e.key === "ArrowDown"){
+      addMove("down")
+    }
+  })
+
+
+  button = document.querySelector('#move-button')
+  
+  // button.addEventListener('click', function()
+  //   const moveContainer = document.querySelector("#moves-container")
+  //   const moveDirection = moveContainer.querySelector("li:first-child")
+  //     move(moveDirection.textContent)
+  //     moveDirection.remove()
+  // })
+
+  // with intervals
+  button.addEventListener('click', function(){ 
+    setInterval(function(){
+      const moveContainer = document.querySelector("#moves-container")
+      const moveDirection = moveContainer.querySelector("li:first-child")
+      if (moveDirection) {
+        move(moveDirection.textContent)
+        moveDirection.remove()}}, 500)
+  })
+
+
 });
+
+
+
+
+function addMove(direction){
+  const moveContainer = document.querySelector("#moves-container")
+  const newLI = document.createElement('li')
+  newLI.classList.add ('move')
+    newLI.textContent = direction
+    moveContainer.append(newLI)
+}
